f639d5617f830dd97bfeb97033f2c7f0b1829150
yhanz99 <85608175+yhanz99@users.noreply.github.com>
2022-06-24 01:18:59 +0800

    Update user_interface2.py

--- user_interface2.py
+++ user_interface2.py
@@ -382,7 +382,7 @@ st.markdown(
 st.sidebar.title('Upload your diabetes .csv data here')
 
 st.sidebar.subheader("☝️ Step 1")
-st.sidebar.markdown("You shuould begin with downloading the .csv diabetes dateset given here by clicking this download button.")
+st.sidebar.markdown("<h6>You shuould begin with downloading the .csv diabetes dateset given here by clicking this download button.</h6>")
 with open("diabetes_data_upload.csv", "rb") as csv_file: CSVbyte = csv_file.read()
 st.sidebar.download_button(
     label="👉Download Diabetes Dataset👈",
