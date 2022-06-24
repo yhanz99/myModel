5d2d519bbc1d24b7f2f6aa733c3c2bc641294ed9
yhanz99 <85608175+yhanz99@users.noreply.github.com>
2022-06-24 15:55:20 +0800

    Update user_interface2.py

--- user_interface2.py
+++ user_interface2.py
@@ -286,7 +286,7 @@ def build_model(DF):
 
     # display accuracy (EM)
     st.markdown(
-        "<h6 style='text-align:left; color:#3A4B74;'><mark style='background-color: #F8BBD0;'>(Section 3 - Part) ⌨Model 3: Ensemble Method Model</mark></h6>",
+        "<h6 style='text-align:left; color:#3A4B74;'><mark style='background-color: #F8BBD0;'>(Section 3 - Part 3) ⌨Model 3: Ensemble Method Model</mark></h6>",
         unsafe_allow_html=True)
     st.write(" **A. The Accuracy Result of Train Set** ", (ec_train_sc * 100).round(2), "%")
     st.write(" **B. The Accuracy Result of Test Set** ", (ec_test_sc * 100).round(2), "%")
@@ -361,7 +361,7 @@ def build_model(DF):
     st.header("👀 Sneak peak on findings of previous research")
     st.markdown("<p style='text-align:;left; color:#054B4A;'><i>This project was conducted on five (5) previous research works from machine learning expertise."
              " I would like to grab this opportunity to share one (1) of the works here. You may download this research paper as your reference too. Hope you enjoy it!"
-                " -Yee Hang</i></p>", unsafe_allow_html= True)
+                "     -Yee Hang</i></p>", unsafe_allow_html= True)
     st.write("\n")
 
     st.markdown("**Details of the research paper:**")
